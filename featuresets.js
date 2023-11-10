08d9e13ae17b61dc964df6a0eb5db5a66d75aa1d
Felix <23635466+its-felix@users.noreply.github.com>
2023-11-10 03:09:29 +0100

    enhance github workflows

---
+++ featuresets.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "beta": {
+    "gw2auth.com-api": ["main"],
+    "gw2auth.com-ui": ["main"],
+    "gw2auth.com-cdk": ["main"],
+    "opentelemetry-lambda": ["main"]
+  },
+  "feature/ui-rework": {
+    "gw2auth.com-api": ["rework"],
+    "gw2auth.com-ui": ["react-cloudscape-rewrite"],
+    "gw2auth.com-cdk": ["main"],
+    "opentelemetry-lambda": ["main"]
+  }
+}
