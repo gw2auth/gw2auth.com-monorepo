cdb0134488a7b4ade3300a5e5948d67e2d00fad2
Felix <23635466+its-felix@users.noreply.github.com>
2024-01-13 06:29:33 +0100

    remove feature/ui-rework branch

--- featuresets.js
+++ featuresets.js
@@ -4,11 +4,5 @@ module.exports = {
     "gw2auth.com-ui": ["main"],
     "gw2auth.com-cdk": ["main"],
     "opentelemetry-lambda": ["main"]
-  },
-  "feature/ui-rework": {
-    "gw2auth.com-api": ["main"],
-    "gw2auth.com-ui": ["react-cloudscape-rewrite"],
-    "gw2auth.com-cdk": ["main"],
-    "opentelemetry-lambda": ["main"]
   }
 }
