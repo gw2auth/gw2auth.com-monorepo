574dc8d757166832792303ffe3b9c79c87722ffb
Felix <23635466+its-felix@users.noreply.github.com>
2024-11-14 04:29:03 +0100

    add canary

--- featuresets.js
+++ featuresets.js
@@ -2,6 +2,7 @@ module.exports = {
   "beta": {
     "gw2auth.com-api": ["main"],
     "gw2auth.com-ui": ["main"],
+    "gw2auth.com-canary": ["main"],
     "gw2auth.com-cdk": ["main"],
     "opentelemetry-lambda": [".*"]
   }
