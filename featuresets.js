969779c58f451078cc40eec7547b343e64982fe7
Felix <23635466+its-felix@users.noreply.github.com>
2025-08-29 03:55:27 +0200

    add background-jobs submodule

--- featuresets.js
+++ featuresets.js
@@ -1,6 +1,7 @@
 module.exports = {
   "beta": {
     "gw2auth.com-api": ["main"],
+    "gw2auth.com-background-jobs": ["main"],
     "gw2auth.com-ui": ["main"],
     "gw2auth.com-canary": ["main"],
     "gw2auth.com-cdk": ["main"],
